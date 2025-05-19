c42470da3dbb2788c39fcbde89e7f478f946f7ef
kiruha <berestkirill89@gmail.com>
2025-05-19 19:53:46 +0300

    first commit

--- jojk.js
+++ jojk.js
@@ -6,31 +6,34 @@ let interval = null;
 let decayInterval = null;
 let color = null
 let light_range = null // ${color}
-let size = ""
+let size = 5
 
 function updateUI() {
     valueDisplay.textContent = `пОтужність: ${potuzhnist} ПТЖ`;
     valueDisplay.style.textShadow = `0px 0px ${light_range}px red`;
+    valueDisplay.style.fontSize = `${size}px`
 }
 function start() {
     clearInterval(interval);
     interval = setInterval(function() {
         if (potuzhnist < 100) {
-            potuzhnist+=5;
+            potuzhnist+=2;
             light_range+=2;
+            size+=5
             updateUI();
           }
-        }, 200);
+        }, 150);
       }
 
 function stop() {
     clearInterval(interval);
     interval = setInterval(function() {
     if (potuzhnist > 0) {
-        potuzhnist-=5;
+        potuzhnist-=2;
         light_range-=2;
+        size+=5
         updateUI();
-    }}, 200);
+    }}, 150);
 }
 
 button.addEventListener("mousedown", start)
